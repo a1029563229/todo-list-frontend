08297e5ba610b92b7583474b6126d02721f138ea
lt <1029563229@qq.com>
2022-03-12 14:29:14 +0800

    调试插件

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import path from 'path';
 import vue from '@vitejs/plugin-vue'
+import markdown from './plugin-markdown/src/index';
 import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 
@@ -16,6 +17,7 @@ export default defineConfig({
   },
   plugins: [
     vue(),
+    markdown(),
     Components({
       resolvers: [
         AntDesignVueResolver(),
